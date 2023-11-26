f22090d958072bcd9906a472f1427351d6d15fc5
Priya Deshmukh <128911820+priya1599@users.noreply.github.com>
2023-11-26 19:23:39 +0530

    Update test.js

--- _tests_/test.js
+++ _tests_/test.js
@@ -12,7 +12,7 @@ describe("API Routes", () => {
   before(async () => {
     // Connect to a test database or perform any setup you need
     // This will depend on your testing environment and strategy
-    await mongoose.connect('mongodb://127.0.0.1:27017/', {
+    await mongoose.connect('mongodb+srv://priyadeshmukh15pd:K7qmZl1W3ljmlrgh@cluster0.ffbiu6a.mongodb.net/', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
